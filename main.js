d5293967ca8e272a2f1bdf65a0df3e9123ccf85e
mardanius <mariojeda.mg@gmail.com>
2022-05-20 21:30:08 -0400

    Created first prototype and first instance of prototype

--- main.js
+++ main.js
@@ -9,4 +9,24 @@ const natalia = {
   }
   
   natalia.aprobarCurso("Curso de JAVA");
-  
+
+  //Creando primer prototipo y primera instancia de prototipo
+
+  const Student = function(name, age, cursosAprobados){
+      this.name = name;
+      this.age = age;
+      this.cursosAprobados = cursosAprobados;
+  }
+
+  Student.prototype.aprobarCurso = function(nuevoCurso){
+      this.cursosAprobados.push(nuevoCurso);
+  }
+
+  const juanita = new Student(
+      "Juana",
+      33,
+      [
+          "POO en PHP",
+          "Curso de Unreal Engine"
+      ]
+    )
