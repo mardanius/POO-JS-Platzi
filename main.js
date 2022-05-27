b62a36a23273b970a72920d4b4c130798ab37007
mardanius <mariojeda.mg@gmail.com>
2022-05-27 12:45:25 -0400

    Added index.html and in the JS file LearningPath and Cursos classes

--- main.js
+++ main.js
@@ -48,3 +48,70 @@ class student2 {
 const carlos = new student2({name: "Carlitox", age: 12, cursosAprobados: ["Curso de piloto aviador", "Curso de oratoria"]})
 
 // Challenge
+
+class LearningPath{
+    constructor({name, courses}){
+        this.name = name;
+        this.courses = courses; 
+    }
+}
+
+class Cursos{
+    constructor({
+        name,
+        lecciones = []
+    }){
+        this._name = name;
+        this.lecciones = lecciones;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(nuevoNombre){
+        if(nuevoNombre === "Curso Malo"){
+            console.error ("Si sos profesional no lo hagas");
+        } else{
+            this._name = nuevoNombre;
+        }
+    }
+}
+
+const cursoProgBasica = new Cursos({
+    name: "Curso de Programación Básica",
+    lecciones: []
+})
+
+const cursoHTML = new Cursos({
+    name: "Curso de HTML Básico",
+    lecciones: []
+})
+
+const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrollo Web",
+    courses: [
+        cursoProgBasica,
+        cursoHTML,
+        "Curso de CSS",
+        "Curso de JS"
+    ]
+})
+
+const escuelaData = new LearningPath({
+    name: "Escuela de Data Science",
+    courses: [
+        cursoProgBasica,
+        "Curso de Python",
+        "Curso de DataBussines"
+    ]
+})
+
+const escuelaVgs = new LearningPath({
+    name: "Escuela de VideoJuegos",
+    courses: [
+        cursoProgBasica,
+        "Curso de Unreal Engine",
+        "Curso de C"
+    ]
+})
